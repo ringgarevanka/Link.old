fac2ced3c161302b9845adc015fb1bfdb4b638c7
Ringga Revanka <99320495+ringgarevanka@users.noreply.github.com>
2023-10-07 19:03:44 +0700

    Update main-modules.js
    test

--- main-modules.js
+++ main-modules.js
@@ -1,50 +1,33 @@
-import {
-	main
-} from "./index.js";
+import { main, links } from "./index.js";
+
 const mainContainer = document.getElementById("main");
-
-function addmain(profilepicture, name, username, hashtag) {
-	return `
-<a id="profilePicture">
-<img src="${profilepicture}">
-</a>
-<div id="name">
-${name}
-</div>
-<div id="username">
-${username}
-</div>
-<div class="links" id="links"></div>
-<div id="hashtag">
-${hashtag}
-</div>
-`
-}
-let allmain = "";
-main.forEach((ele => {
-	let profilepicture = ele.profilepicture,
-		name = ele.name,
-		username = ele.username,
-		hashtag = ele.hashtag;
-	allmain += addmain(profilepicture, name, username, hashtag)
-})), mainContainer.innerHTML = allmain;
-
-import {
-	links
-} from "./index.js";
 const linkContainer = document.getElementById("links");
 
-function addLink(inputname, inputlink, inputicon) {
-	return `
-<a class="link" href="${inputlink}" target="_blank">
-<i class="${inputicon}"></i> ${inputname}
-</a>
-`
+function addMain(profilepicture, name, username, hashtag) {
+  return `
+    <a id="profilePicture">
+      <img src="${profilepicture}">
+    </a>
+    <div id="name">
+      ${name}
+    </div>
+    <div id="username">
+      ${username}
+    </div>
+    <div class="links" id="links"></div>
+    <div id="hashtag">
+      ${hashtag}
+    </div>
+  `;
 }
-let allLinks = "";
-links.forEach((ele => {
-	let inputlink = ele.inputlink,
-		inputname = ele.inputname,
-		inputicon = ele.inputicon;
-	allLinks += addLink(inputname, inputlink, inputicon)
-})), linkContainer.innerHTML = allLinks;
+
+function addLink(inputname, inputlink, inputicon) {
+  return `
+    <a class="link" href="${inputlink}" target="_blank">
+      <i class="${inputicon}"></i> ${inputname}
+    </a>
+  `;
+}
+
+mainContainer.innerHTML = main.map(ele => addMain(ele.profilepicture, ele.name, ele.username, ele.hashtag)).join('');
+linkContainer.innerHTML = links.map(ele => addLink(ele.inputname, ele.inputlink, ele.inputicon)).join('');
