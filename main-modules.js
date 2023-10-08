b700df6d491f6deee0243b5f3316becc6baa7ba0
Ringga Revanka <99320495+ringgarevanka@users.noreply.github.com>
2023-10-08 12:06:14 +0700

    Update main-modules.js

--- main-modules.js
+++ main-modules.js
@@ -1,40 +1,51 @@
-import main, links from "./index.js";
-
-const mainContainer = document.getElementById("main");
-const linkContainer = document.getElementById("links");
-
-function addMain(profilepicture, name, username, hashtag) {
-  return `
-    <a id="profilePicture">
-      <img src="${profilepicture}">
-    </a>
-    <div id="name">
-      ${name}
-    </div>
-    <div id="username">
-      ${username}
-    </div>
-    <div class="links" id="links"></div>
-    <div id="hashtag">
-      ${hashtag}
-    </div>`;
-}
-
-function addLink(inputname, inputlink, inputicon) {
-  return `
-    <a class="link" href="${inputlink}" target="_blank">
-      <i class="${inputicon}"></i> ${inputname}
-    </a>`;
-}
-
-let allMain = "";
-main.forEach((ele) => {
-  allMain += addMain(ele.profilepicture, ele.name, ele.username, ele.hashtag);
-});
-mainContainer.innerHTML = allMain;
-
-let allLinks = "";
-links.forEach((ele) => {
-  allLinks += addLink(ele.inputname, ele.inputlink, ele.inputicon);
-});
-linkContainer.innerHTML = allLinks;
+//TEMPORARY CODE
+import { 
+         main 
+ } from "./index.js"; 
+ const mainContainer = document.getElementById("main"); 
+  
+ function addmain(profilepicture, name, username, hashtag) { 
+         return ` 
+ <a id="profilePicture"> 
+ <img src="${profilepicture}"> 
+ </a> 
+ <div id="name"> 
+ ${name} 
+ </div> 
+ <div id="username"> 
+ ${username} 
+ </div> 
+ <div class="links" id="links"></div> 
+ <div id="hashtag"> 
+ ${hashtag} 
+ </div> 
+ ` 
+ } 
+ let allmain = ""; 
+ main.forEach((ele => { 
+         let profilepicture = ele.profilepicture, 
+                 name = ele.name, 
+                 username = ele.username, 
+                 hashtag = ele.hashtag; 
+         allmain += addmain(profilepicture, name, username, hashtag) 
+ })), mainContainer.innerHTML = allmain; 
+  
+ import { 
+         links 
+ } from "./index.js"; 
+ const linkContainer = document.getElementById("links"); 
+  
+ function addLink(inputname, inputlink, inputicon) { 
+         return ` 
+ <a class="link" href="${inputlink}" target="_blank"> 
+ <i class="${inputicon}"></i> ${inputname} 
+ </a> 
+ ` 
+ } 
+ let allLinks = ""; 
+ links.forEach((ele => { 
+         let inputlink = ele.inputlink, 
+                 inputname = ele.inputname, 
+                 inputicon = ele.inputicon; 
+         allLinks += addLink(inputname, inputlink, inputicon) 
+ })), linkContainer.innerHTML = allLinks;
