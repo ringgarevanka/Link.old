78c371a6dd0c64b4428a336a20a22325eae07101
Ringga Revanka <99320495+ringgarevanka@users.noreply.github.com>
2023-12-07 19:55:51 +0700

    Update script.js (v 1.0_071220231955)
    add some element

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function preloadFiles() {
     './script.js',
     './app.js',
     './data.js',
+    'https://avatars.githubusercontent.com/u/99320495',
     'https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css',
     'https://cdn.discordapp.com/attachments/1182294906421645464/1182295005298184232/compresed.png',
