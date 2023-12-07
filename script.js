07f2c2c09917560226b4c81731a26b6728e3f1b7
Ringga Revanka <99320495+ringgarevanka@users.noreply.github.com>
2023-12-07 19:42:49 +0700

    Update script.js (v 1.0_071220231942)
    + add preload !test!

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-/* NONE */
+/* Preload Files */
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("All files loaded. Displaying the page!");
+});
+
+function preloadFiles() {
+  var fileUrls = [
+    './index.html',
+    './style.css',
+    './script.js',
+    './app.js',
+    './data.js',
+    'https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins&display=swap',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css',
+    'https://cdn.discordapp.com/attachments/1182294906421645464/1182295005298184232/compresed.png',
+  ];
+
+  var loadedFilesCount = 0;
+
+  function fileLoaded() {
+    loadedFilesCount++;
+
+    if (loadedFilesCount === fileUrls.length) {
+      var event = new Event("DOMContentLoaded");
+      document.dispatchEvent(event);
+    }
+  }
+
+  fileUrls.forEach(function (url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        fileLoaded();
+      } else {
+        console.error('Failed to load: ' + url);
+      }
+    };
+
+    xhr.send();
+  });
+}
+
+preloadFiles();
