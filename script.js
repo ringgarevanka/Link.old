3a2fba3cca5d936354fedebffc82f13389371928
Ringga Revanka <99320495+ringgarevanka@users.noreply.github.com>
2023-12-23 11:15:36 +0700

    Update script.js
    preload update

--- script.js
+++ script.js
@@ -1,40 +1,34 @@
 /* Preload Files */
-function preloadFiles() {
-  var fileUrls = [
-    './index.html',
-    './style.css',
-    './script.js',
-    './app.js',
-    './data.js',
-    'https://avatars.githubusercontent.com/u/99320495',
-    'https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins&display=swap',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css',
-    'https://cdn.discordapp.com/attachments/1182294906421645464/1182295005298184232/compresed.png',
-  ];
-
-  var loadedFilesCount = 0;
-
-  function fileLoaded() {
-    loadedFilesCount++;
-
-    if (loadedFilesCount === fileUrls.length) {
-      var event = new Event("DOMContentLoaded");
-      document.dispatchEvent(event);
-    }
-  }
-
-  fileUrls.forEach(function (url) {
-    fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Failed to load: ' + url);
-        }
-        fileLoaded();
-      })
-      .catch(error => {
-        console.error(error.message);
-      });
-  });
+function preload(src) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", src);
+  xhr.responseType = "arraybuffer";
+  xhr.onload = () => {
+    // Store resources in cache
+    const cache = window.localStorage;
+    cache.setItem(src, xhr.response);
+  };
+  xhr.send();
 }
 
-preloadFiles();
+preload("/app.js");
+preload("/data.js");
+preload("/style.css");
+preload("https://avatars.githubusercontent.com/u/99320495");
+preload("https://cdn.discordapp.com/attachments/1182294906421645464/1182295005298184232/compresed.png");
+preload("https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins&display=swap");
+preload("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css");
+/*
+preload("url");
+preload("url");
+preload("url");
+preload("url");
+preload("url");
+preload("url");
+preload("url");
+preload("url");
+preload("url");
+preload("url");
+preload("url");
+preload("url");
+*/
